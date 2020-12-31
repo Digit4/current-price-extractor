d4e455d881a1bc0fec61015d81873684b52b006a
Dhaval Pantojee <dpantojee775@gmail.com>
2020-12-31 14:52:44 +0530

    Created server which takes exchange:ticker and gives current stock price from finance.google.com

---
+++ app.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup as soup
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World"
+
+@app.route('/<exchange>/<ticker>')
+def getTickerData(exchange,ticker):
+    # exchange="BSE"
+    # ticker="INFY"
+    base_url = "https://www.google.com/finance/search?tbm=fin&q="
+    response = requests.get(f"{base_url}{exchange}:{ticker}")
+
+    page_soup = soup(response.text,'html.parser')
+
+    price_container = page_soup.findAll("div",{"class":"BNeawe iBp4i AP7Wnd"})
+    if(len(price_container)!=0):
+        return(price_container[0].text.split(" ")[0].replace(",",""))
+    else:
+        return None
