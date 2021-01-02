a5d3d2351f1d0d14495551435d3783bc2725dc9a
Dhaval Pantojee <dpantojee775@gmail.com>
2021-01-02 13:21:50 +0530

    Added cors headers to flask backend to fix 500 errors on frontend

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup as soup
 from flask import Flask
+from flask_cors import CORS
 import requests
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
