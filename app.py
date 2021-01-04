90567933177e12f3dcefcc759a1d54e4c0aebe0d
Dhaval Pantojee <dpantojee775@gmail.com>
2021-01-04 16:13:45 +0530

    everything in try catch block

--- app.py
+++ app.py
@@ -12,15 +12,26 @@ def hello_world():
 
 @app.route('/<exchange>/<ticker>')
 def getTickerData(exchange,ticker):
-    # exchange="BSE"
-    # ticker="INFY"
+
     base_url = "https://www.google.com/finance/search?tbm=fin&q="
-    response = requests.get(f"{base_url}{exchange}:{ticker}")
+    try:
+        response = requests.get(f"{base_url}{exchange}:{ticker}")
+    except Exception as exc:
+        return exc
 
-    page_soup = soup(response.text,'html.parser')
+    try:
+        page_soup = soup(response.text,'html.parser')
+    except Exception as exc:
+        return exc
 
-    price_container = page_soup.findAll("div",{"class":"BNeawe iBp4i AP7Wnd"})
-    if(len(price_container)!=0):
-        return(price_container[0].text.split(" ")[0].replace(",",""))
-    else:
-        return "-1"
+    try:
+        price_container = page_soup.findAll("div",{"class":"BNeawe iBp4i AP7Wnd"})
+    except Exception as exc:
+        return exc
+    try:
+        if(len(price_container)!=0):
+            return(price_container[0].text.split(" ")[0].replace(",",""))
+        else:
+            return "-1"
+    except Exception as exc:
+        return exc
